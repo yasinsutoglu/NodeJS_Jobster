88aae28d34f14d40559d80c2aa3675f4ae6f17b9
Yasin Sutoglu <ysutoglu@gmail.com>
2023-03-28 10:04:50 +0300

    stats update done

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -138,10 +138,27 @@ const showStats = (req, res) => {
     declined: stats.declined || 0,
   };
 
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ]);
 
+  monthlyApplications = monthlyApplications.map((item) => {
+      const {_id: { year, month }, count} = item;
+      const date = moment().month(month - 1).year(year).format('MMM Y');
+      return { date, count };
+    }).reverse();
 
+  // console.log(monthlyApplications)
 
-  res.status(StatusCodes.OK).json({ defaultStats , monthlyApplications: [] });
+  res.status(StatusCodes.OK).json({ defaultStats , monthlyApplications });
 };
 
 module.exports = {
